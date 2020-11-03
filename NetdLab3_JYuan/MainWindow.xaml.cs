0fef3df37c1381ff62aba62c04d4c0017aa9c74b
jacky <jacky.yuan@dcmail.ca>
2020-11-02 22:38:42 -0500

    Finished view Entries page, completed create entry page, view summary page missing revenue.

--- NetdLab3_JYuan/MainWindow.xaml.cs
+++ NetdLab3_JYuan/MainWindow.xaml.cs
@@ -18,7 +18,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using System.Data;
 using System.Data.SqlClient;
 
 namespace NetdLab3_JYuan
@@ -31,9 +31,87 @@ namespace NetdLab3_JYuan
         public MainWindow()
         {
             InitializeComponent();
+            FillDataGrid();
+            FillSummary();
+        }
+        /// <summary>
+        /// Function used for filling the data grid for view entries
+        /// </summary>
+        private void FillDataGrid()
+        {
+            try
+            { //Connect to data source
+                string connectString = Properties.Settings.Default.connect_string;
+                SqlConnection cn = new SqlConnection(connectString);
+                cn.Open();
+                //This is the SQL query we want to run.
+                string selectionQuery = "SELECT * FROM entries";
+                SqlCommand command = new SqlCommand(selectionQuery, cn);
+                SqlDataAdapter sda = new SqlDataAdapter(command);
+                //import the table
+                DataTable dt = new DataTable("PurchaseEntries");
+                sda.Fill(dt);
+                //Bind datatable to the sql table
+                dtgOutputDisplay.ItemsSource = dt.DefaultView;
+            }
+            catch (Exception ex)
+            {
+                //Shows error in message box.
+                MessageBox.Show(ex.ToString());
+            }
+
         }
 
-        private void btnCreateEntry_Click(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// Function used to fill the summary page
+        /// </summary>
+        private void FillSummary()
+        {
+            try
+            {
+                //Connect to data source
+                string connectString = Properties.Settings.Default.connect_string;
+                SqlConnection cn = new SqlConnection(connectString);
+                cn.Open();
+                //This is the SQL query we want to run.
+                string selectionQuery = "SELECT * FROM shares";
+                SqlCommand command = new SqlCommand(selectionQuery, cn);
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    reader.Read();
+                    txtCommonAvailable.Text = reader.GetInt32(0).ToString();
+                    txtPreferredAvailable.Text = reader.GetInt32(1).ToString();
+                }
+                selectionQuery = "SELECT SUM(shares) FROM entries WHERE shareType= \'Common\'";
+                command = new SqlCommand(selectionQuery, cn);
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    reader.Read();
+                    txtCommonSold.Text = reader.GetInt32(0).ToString();
+                }
+                selectionQuery = "SELECT SUM(shares) FROM entries WHERE shareType= \'Preferred\'";
+                command = new SqlCommand(selectionQuery, cn);
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    reader.Read();
+                    txtPreferredSold.Text = reader.GetInt32(0).ToString();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                //Shows error in message box.
+                MessageBox.Show(ex.ToString());
+            }
+
+        }
+
+            /// <summary>
+            /// Event handler for button press of create entry
+            /// </summary>
+            /// <param name="sender"></param>
+            /// <param name="e"></param>
+            private void btnCreateEntry_Click(object sender, RoutedEventArgs e)
         {
             int numberofCommons;
             int numberofPreferred;
@@ -42,23 +120,16 @@ namespace NetdLab3_JYuan
             SqlConnection cn = new SqlConnection(connectString);
             cn.Open();
             //create new query statement
-            string insertQuery = "SELECT numPreferredShares FROM shares";
+            string insertQuery = "SELECT numCommonShares, numPreferredShares FROM shares";
             SqlCommand command = new SqlCommand(insertQuery, cn);
             //read query data and insert into variable
             using (SqlDataReader reader = command.ExecuteReader())
             {
                 reader.Read();
-                numberofPreferred = reader.GetInt32(0);
-            }
-            //create new query statement
-            insertQuery = "SELECT numCommonShares FROM shares";
-            command = new SqlCommand(insertQuery, cn);
-            //read query data and insert into variable
-            using (SqlDataReader reader = command.ExecuteReader())
-            {
-                reader.Read();
                 numberofCommons = reader.GetInt32(0);
+                numberofPreferred = reader.GetInt32(1);
             }
+
             int tempint;
             string TypeofShare;
             //checks if the name field is empty
@@ -92,6 +163,12 @@ namespace NetdLab3_JYuan
                                     cn.Close();
                                     //Messagebox for a successful input
                                     MessageBox.Show("The entry has been submitted");
+                                    //resets the form
+                                    txtBuyerName.Text = " ";
+                                    txtShareNo.Text = " ";
+                                    dtpDatePicker.SelectedDate = null;
+
+
                                 }
                                 //not enough for the desired amount of shares
                                 else
@@ -122,6 +199,12 @@ namespace NetdLab3_JYuan
                                     cn.Close();
                                     //Messagebox for a successful input
                                     MessageBox.Show("The entry has been submitted");
+                                    //resets the form
+                                    txtBuyerName.Text = " ";
+                                    txtShareNo.Text = " ";
+                                    dtpDatePicker.SelectedDate = null;
+                                    radCommon.IsChecked = true;
+                                    radPreferred.IsChecked = false;
                                 }
                                 //not enough for the desired amount of shares
                                 else
